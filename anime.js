5e48ac1412f3d02ebb744aa8ad6cc19e45f46e4d
coderner <shreyashukla680@gmail.com>
2021-10-06 01:58:59 +0530

    added animation to ant

--- anime.js
+++ anime.js
@@ -1,11 +1,32 @@
 var man = document.querySelector(".image");
-var man_pos= screen.width-300;
-var time = setInterval(move,1);
-function move(){
-    man_pos--;
-    man.style.left= man_pos+'px';
-   if(parseInt(man.style.left)==(screen.width-1300))
-   {
-    clearInterval(time);
-   }
+var ant = document.querySelector(".antmain");
+
+
+
+var man_pos= screen.width-250;
+var time = setInterval(moveman,1);
+function moveman()
+{
+      man_pos--;
+      man.style.left= man_pos+'px';
+     if(parseInt(man.style.left)==(screen.width-1350))
+     {
+        clearInterval(time);
+        var time2 = setInterval(moveant,0.5);
+        var ant_width= screen.width-650;
+        var ant_height= screen.height;
+        function moveant()
+         {
+            ant_width--;
+            ant_height--;
+            ant.style.left= ant_width+'px';
+            ant.style.top= ant_height+'px';
+            if((parseInt(ant.style.left)==(screen.width-1100)))
+            {
+                clearInterval(time2);
+                document.getElementsByClassName("message").innerHTML="Treat Time Guys!!"; 
+            }
+         }
+   
+     }
 }
